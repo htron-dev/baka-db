efe0da0145fdf4d18f1d944350e6dfb3e81067e4
Henrique <henryqueoli@hotmail.com>
2021-07-27 23:26:32 -0300

    feat(tests): add tile and date tests

--- japaFile.ts
+++ japaFile.ts
@@ -1,4 +1,7 @@
+import moment from "moment";
 import { configure } from "japa";
+
+moment.suppressDeprecationWarnings = true;
 
 configure({
     files: ["tests/*.test.ts"],
--- tests/catalog.test.ts
+++ tests/catalog.test.ts
@@ -1,4 +1,5 @@
 import test from "japa";
+import moment from "moment";
 
 import { createCatalogManger } from "../src/catalog-manager";
 
@@ -10,11 +11,49 @@ test.group("test markdown files", (group) => {
 
     group.before(async () => {
         const manager = createCatalogManger();
-
+        // TODO: get only en_US files
         items = await manager.getItems();
     });
 
-    test("should item type be valid", (assert) => {
+    test("should items have title", (assert) => {
+        items.forEach((item) => {
+            assert.equal(
+                item.title && item.title !== "",
+                true,
+                `Title is required`
+            );
+        });
+    });
+
+    test("should items start-date be valid if have one", (assert) => {
+        items.forEach((item) => {
+            if (!item.start_date) {
+                return;
+            }
+
+            assert.equal(
+                moment(item.start_date).isValid(),
+                true,
+                `Invalid date "${item.start_date}"`
+            );
+        });
+    });
+
+    test("should items end-date be valid if have one", (assert) => {
+        items.forEach((item) => {
+            if (!item.end_date) {
+                return;
+            }
+
+            assert.equal(
+                moment(item.end_date).isValid(),
+                true,
+                `Invalid date "${item.end_date}"`
+            );
+        });
+    });
+
+    test("should items type be valid", (assert) => {
         items.forEach((item) => {
             assert.equal(
                 validTypes.includes(item.type),
@@ -24,7 +63,7 @@ test.group("test markdown files", (group) => {
         });
     });
 
-    test("should item tags be valid", (assert) => {
+    test("should items tags be valid", (assert) => {
         items
             .map((item) => item.tags || [])
             .reduce((all, tags) => all.concat(tags), [])
