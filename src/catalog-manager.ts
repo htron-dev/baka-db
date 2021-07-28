4f73bb1bb5ea147ed6eeab758ea8cf7a624f8c70
Henrique <henryqueoli@hotmail.com>
2021-07-28 01:40:39 -0300

    feat(tests): add option to use specific filenames

--- src/catalog-manager.ts
+++ src/catalog-manager.ts
@@ -4,10 +4,14 @@ import { promisify } from 'util'
 
 import { convertFileToObject } from './convert-to-object'
 
+export interface Filters {
+    lang?: string
+}
+
 export function createCatalogManger() {
     const catalogPath = path.resolve(__dirname, '..', 'catalog')
 
-    async function getFilePaths() {
+    async function getFilePaths(filters?: Filters) {
         const projects = await promisify(fs.readdir)(catalogPath)
 
         const projectPaths = await Promise.all(
@@ -16,16 +20,22 @@ export function createCatalogManger() {
                     path.resolve(catalogPath, project)
                 )
 
-                return files.map((f) => path.resolve(catalogPath, project, f))
+                return files
+                    .filter((f) => {
+                        if (filters?.lang) {
+                            const [lang] = f.split('_')
+                            return lang === filters.lang
+                        }
+                        return true
+                    })
+                    .map((f) => path.resolve(catalogPath, project, f))
             })
         )
 
         return projectPaths.reduce((all, p) => all.concat(p), [])
     }
 
-    async function getItems() {
-        const filesPaths = await getFilePaths()
-
+    async function getItems(filesPaths: string[]) {
         return await Promise.all(filesPaths.map(convertFileToObject))
     }
 
--- src/tests/catalog.test.ts
+++ src/tests/catalog.test.ts
@@ -1,69 +1,83 @@
-import test from "japa";
-import moment from "moment";
+import test from 'japa'
+import moment from 'moment'
+import path from 'path'
 
-import { createCatalogManger } from "../catalog-manager";
+import { createCatalogManger } from '../catalog-manager'
 
-import validTypes from "../valid-types.json";
-import validTags from "../valid-tags.json";
+import validTypes from '../valid-types.json'
+import validTags from '../valid-tags.json'
 
-test.group("test markdown files", (group) => {
-    let items: any[] = [];
+test.group('Markdown en-US files content', (group) => {
+    let items: any[] = []
 
     group.before(async () => {
-        const manager = createCatalogManger();
-        // TODO: get only en_US files
-        items = await manager.getItems();
-    });
+        const manager = createCatalogManger()
 
-    test("should items have title", (assert) => {
+        let changedFiles = process.argv.slice(2)
+
+        if (!changedFiles.length) {
+            changedFiles = await manager.getFilePaths({
+                lang: 'en-US'
+            })
+        }
+
+        const filePaths = changedFiles.filter((filename) => {
+            const [lang] = path.basename(filename).split('_')
+            return lang === 'en-US'
+        })
+
+        items = await manager.getItems(filePaths)
+    })
+
+    test('should items have title', (assert) => {
         items.forEach((item) => {
             assert.equal(
-                item.title && item.title !== "",
+                item.title && item.title !== '',
                 true,
-                "Title is required"
-            );
-        });
-    });
+                `Title is required`
+            )
+        })
+    })
 
-    test("should items start-date be valid if have one", (assert) => {
+    test('should items start-date be valid if have one', (assert) => {
         items.forEach((item) => {
             if (!item.start_date) {
-                return;
+                return
             }
 
             assert.equal(
                 moment(item.start_date).isValid(),
                 true,
                 `Invalid date "${item.start_date}"`
-            );
-        });
-    });
+            )
+        })
+    })
 
-    test("should items end-date be valid if have one", (assert) => {
+    test('should items end-date be valid if have one', (assert) => {
         items.forEach((item) => {
             if (!item.end_date) {
-                return;
+                return
             }
 
             assert.equal(
                 moment(item.end_date).isValid(),
                 true,
                 `Invalid date "${item.end_date}"`
-            );
-        });
-    });
+            )
+        })
+    })
 
-    test("should items type be valid", (assert) => {
+    test('should items type be valid', (assert) => {
         items.forEach((item) => {
             assert.equal(
                 validTypes.includes(item.type),
                 true,
                 `Invalid type "${item.type}"`
-            );
-        });
-    });
+            )
+        })
+    })
 
-    test("should items tags be valid", (assert) => {
+    test('should items tags be valid', (assert) => {
         items
             .map((item) => item.tags || [])
             .reduce((all, tags) => all.concat(tags), [])
@@ -72,7 +86,7 @@ test.group("test markdown files", (group) => {
                     validTags.includes(tag),
                     true,
                     `Invalid tag "${tag}"`
-                );
-            });
-    });
-});
+                )
+            })
+    })
+})
