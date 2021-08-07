0d7ce26f8d1a75bc702a29a609fdd706ca092795
Henrique <henryqueoli@hotmail.com>
2021-08-07 16:23:57 -0300

    fix(github): use redis object config in queue

--- src/run-conversor-json.ts
+++ src/run-conversor-json.ts
@@ -59,9 +59,11 @@ async function main() {
     return new Promise((resolve, reject) => {
         try {
             const host = process.env.REDIS_HOST || '127.0.0.1'
-            const port = process.env.REDIS_PORT || 6379
+            const port = Number(process.env.REDIS_PORT) || 6379
 
-            const queue = new Queue('conversor', `redis://${host}:${port}`)
+            const queue = new Queue('conversor', {
+                redis: { host, port }
+            })
 
             queue.empty()
             queue.clean(0)
